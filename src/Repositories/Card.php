9b203c951e7f24ed6d3d06a915e766247b36bf4c
ali <aldwiri@eaa.com.sa>
2017-05-04 14:39:09 +0300

    Cards Data

---
+++ src/Repositories/Card.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ali
+ * Date: 04/05/17
+ * Time: 01:50 م
+ */
+
+namespace Adwair\PropertyFinder\Repositories;
+
+
+class Card
+{
+    /**
+     * The cards array.
+     *
+     * @var array
+     */
+    private $cards = [
+        [
+            "Departure"             => "Gerona Airport",
+            "Arrival"               => "Stockholm",
+            "Transportation"        => "Plane",
+            "Transportation_number" => "SK455",
+            "Seat"                  => "3A",
+            "Gate"                  => "45B",
+            "Baggage"               => "334",
+        ],
+        [
+            "Departure"      => "Barcelona",
+            "Arrival"        => "Gerona Airport",
+            "Transportation" => "Bus",
+        ],
+        [
+            "Departure"             => "Madrid",
+            "Arrival"               => "Barcelona",
+            "Transportation"        => "Train",
+            "Transportation_number" => "78A",
+            "Seat"                  => "45B",
+        ],
+        [
+            "Departure"             => "Stockholm",
+            "Arrival"               => "New York",
+            "Transportation"        => "Plane",
+            "Transportation_number" => "SK22",
+            "Seat"                  => "7B",
+            "Gate"                  => "22",
+        ],
+    ];
+
+    /**
+     * Cards constructor.
+     * You can use your own cards array rather than the default one
+     *
+     * @param array $cards
+     */
+    public function __construct($cards = [])
+    {
+        if (count($cards)) {
+            $this->cards = $cards;
+        }
+    }
+
+    /**
+     * get the journey cards.
+     *
+     * @return array
+     */
+    public function getCards()
+    {
+        return $this->cards;
+    }
+}
