d24fc465a008b09f3f7baae092f777a3f6ef351f
ali <aldwiri@eaa.com.sa>
2017-05-04 23:28:25 +0300

    README and Unit test

---
+++ API.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ali
+ * Date: 04/05/17
+ * Time: 11:16 م
+ */
+
+/**
+ * Disallow direct access to the file
+ */
+if (count(get_included_files()) == 1) exit("Direct access not permitted. Please refer to the docs provided with your API");
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Adwairi\PropertyFinder\Sorter;
+
+/**
+ * Class Trip
+ */
+class Trip{
+
+    private $trip;
+    public function __construct($cards = [])
+    {
+        $sorter   = new Sorter($cards);
+        $this->trip = $sorter->buildJourney();
+    }
+
+    public function getTrip(){
+        return $this->trip;
+    }
+
+}
--- index.php
+++ index.php
@@ -6,13 +6,41 @@
  * Time: 03:12 م
  */
 
-// Composer autoload
-use Adwairi\PropertyFinder\Sorter;
+include_once('API.php');
 
-require_once __DIR__ . '/vendor/autoload.php';
+$cards = [
+    [
+        "Departure"             => "Gerona Airport",
+        "Arrival"               => "Stockholm",
+        "Transportation"        => "Plane",
+        "Transportation_number" => "SK455",
+        "Seat"                  => "3A",
+        "Gate"                  => "45B",
+        "Baggage"               => "334",
+    ],
+    [
+        "Departure"      => "Barcelona",
+        "Arrival"        => "Gerona Airport",
+        "Transportation" => "Bus",
+    ],
+    [
+        "Departure"             => "Madrid",
+        "Arrival"               => "Barcelona",
+        "Transportation"        => "Train",
+        "Transportation_number" => "78A",
+        "Seat"                  => "45B",
+    ],
+    [
+        "Departure"             => "Stockholm",
+        "Arrival"               => "New York",
+        "Transportation"        => "Plane",
+        "Transportation_number" => "SK22",
+        "Seat"                  => "7B",
+        "Gate"                  => "22",
+    ],
+];
 
-$sorter   = new Sorter();
-$journey_desc = $sorter->buildJourney();
-
+$trip = new Trip($cards);
+$journey_desc = $trip->getTrip();
 echo "<pre>" . $journey_desc . "</pre>";
 
